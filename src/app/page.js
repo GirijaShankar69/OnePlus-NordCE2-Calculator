ea897c23a4d2f5084054825e80b4571f44615f8c
Chinmay Sa <68277286+ScreenTechnicals@users.noreply.github.com>
2023-06-18 02:56:29 +0530

    Final Build Completed version1 - 1st commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,195 @@
-import Image from 'next/image'
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+import { FaBackspace, FaEquals } from "react-icons/fa";
+import { IoCloseSharp } from "react-icons/io5";
+import { RiDivideLine } from "react-icons/ri";
 
 export default function Home() {
+  const numbers = ["00", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "."];
+
+  const [solve, setSolve] = useState("");
+  const [count, setCount] = useState(0);
+  const [isSolved, setIsSolved] = useState(false);
+
+  var result = null;
+  const clear = async () => {
+    let arr = solve.split("");
+    let x = arr.pop();
+    if (x === ".") {
+      setCount(0);
+    }
+    setSolve(arr.join(""));
+  };
+
+  const operations = [
+    {
+      icon: <RiDivideLine />,
+      value: "/",
+    },
+    {
+      icon: <IoCloseSharp />,
+      value: "*",
+    },
+    {
+      icon: <AiOutlineMinus />,
+      value: "-",
+    },
+    {
+      icon: <AiOutlinePlus />,
+      value: "+",
+    },
+  ];
+
+  const cacluatePercentage = () => {
+    let arr = solve.split("%");
+    let x = 1;
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = arr[i] / 100;
+      setSolve(solve + "" + arr[i]);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="pt-24 pb-20 flex w-full items-center justify-center h-screen bg-[#ffb515]">
+      <div className="mx-auto w-[320px] md:w-[420px] bg-black pb-5 overflow-hidden shadow-2xl shadow-[#000000] rounded-3xl">
+        <div className=" bg-[#808080] rounded-b-3xl w-full h-[200px] md:h-[250px] overflow-y-hidden overflow-x-auto flex justify-end items-end p-5 text-4xl font-[600] relative">
+          <p
+            className={
+              solve?.length
+                ? "absolute top-5 left-5 font-[300] scale-y-0 transition-all duration-500"
+                : "absolute top-5 left-5 font-[300] transition-all duration-500"
+            }
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Calculator
+          </p>
+          <span className={solve?.length > 16 ? "text-xl" : ""}>{solve}</span>
+        </div>
+        <div className="w-full flex px-[10px] py-5 items-start text-white font-[600] text-3xl">
+          <div className="w-[80%] flex items-center flex-wrap">
+            <button
+              onClick={() => {
+                setSolve("");
+                setCount(0);
+              }}
+              className="m-[10px] w-[50px] md:w-[80px] h-[50px] md:h-[80px] rounded-full hover:bg-[#3e3e3e] transition-all duration-500"
+            >
+              AC
+            </button>
+            <button
+              onClick={clear}
+              className="m-[10px] w-[50px] md:w-[80px] h-[50px] md:h-[80px] rounded-full hover:bg-[#3e3e3e] transition-all duration-500 flex items-center justify-center text-3xl"
+            >
+              <FaBackspace />
+            </button>
+            <button
+              onClick={() => {
+                setSolve(solve / 100 + "*");
+              }}
+              className="m-[10px] w-[50px] md:w-[80px] h-[50px] md:h-[80px] rounded-full hover:bg-[#3e3e3e] transition-all duration-500"
+            >
+              %
+            </button>
+            {numbers
+              ?.sort((a, b) => {
+                return b - a;
+              })
+              ?.map((num, index) => (
+                <button
+                  onClick={() => {
+                    if (isSolved) {
+                      setSolve("");
+                      setCount(0);
+                      setIsSolved(false);
+                      setSolve(num + "");
+                      return;
+                    }
+                    if (num === ".") {
+                      if (count === 0) {
+                        setCount(count + 1);
+                        setSolve(solve + "" + num);
+                      }
+                    } else {
+                      setSolve(solve + "" + num);
+                    }
+                  }}
+                  key={index}
+                  className="m-[10px] w-[50px] md:w-[80px] h-[50px] md:h-[80px] rounded-full hover:bg-[#3e3e3e] transition-all duration-500"
+                >
+                  {num}
+                </button>
+              ))}
+          </div>
+          <div className="space-y-5">
+            {operations?.map((op, index) => (
+              <button
+                key={index}
+                onClick={() => {
+                  let arr = solve.toString().split("");
+                  if (
+                    arr[arr.length - 1] !== "+" &&
+                    arr[arr.length - 1] !== "-" &&
+                    arr[arr.length - 1] !== "*" &&
+                    arr[arr.length - 1] !== "/"
+                  ) {
+                    setCount(0);
+                    setSolve(solve + op?.value);
+                  } else if (
+                    (arr[arr.length - 1] === "*" ||
+                      arr[arr.length - 1] === "/") &&
+                    op.value === "-"
+                  ) {
+                    setCount(0);
+                    setSolve(solve + op?.value);
+                  } else if (
+                    !(
+                      arr[arr.length - 1] !== "+" &&
+                      arr[arr.length - 1] !== "-" &&
+                      arr[arr.length - 1] !== "*" &&
+                      arr[arr.length - 1] !== "/"
+                    )
+                  ) {
+                    setCount(0);
+                    arr[arr.length - 1] = op.value;
+                    setSolve(arr.join(""));
+                  }
+                }}
+                className="m-[10px] w-[50px] md:w-[80px] h-[50px] bg-[#3e3e3e] flex text-4xl items-center justify-center md:h-[80px] rounded-full"
+              >
+                {op.icon}
+              </button>
+            ))}
+
+            <button
+              onClick={() => {
+                let arr = solve.toString().split("");
+                try {
+                  if (
+                    arr[arr.length - 1] !== "+" &&
+                    arr[arr.length - 1] !== "-" &&
+                    arr[arr.length - 1] !== "*" &&
+                    arr[arr.length - 1] !== "/" &&
+                    solve?.length > 0
+                  ) {
+                    setIsSolved(true);
+                    result = eval(solve);
+                    setSolve(result.toString());
+                  } else {
+                    alert("Invalid input operation!");
+                  }
+                } catch (err) {
+                  alert("Invalid input operation!");
+                }
+              }}
+              className="m-[10px] w-[50px] md:w-[80px] h-[50px] bg-orange-500 flex items-center justify-center md:h-[80px] rounded-full"
+            >
+              <FaEquals />
+            </button>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
